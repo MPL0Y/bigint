eafe572eb31f629f634b62e0396e728df7cdf31a
Hardik Sharma <handy.sharma121@gmail.com>
2020-05-05 17:28:47 +0530

    First test commit

---
+++ bigint.h
@@ -0,0 +1,27 @@
+/*
+ * bigint: a cpp header file for
+ *		   numbers of arbitrary
+ *		   length.
+ *
+ * Creator: @shardic1
+ */
+
+#ifndef BIG_INT
+#define BIG_INT
+
+//main class definition
+class bigint
+{
+
+public:
+
+	//
+	
+	//constructors
+	bigint(bigint a)
+	{
+
+	}
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "bigint.h"
+using namespace std;
+
+int main()
+{
+	freopen("inputcpp.txt", "r", stdin);
+	freopen("outputcpp.txt", "w", stdout);
+	int a, b;
+	cin >> a >> b;
+	cout << a*b;
+	return 0;
+}
