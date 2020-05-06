c330384ecebc4ad47e0e2eefe208ef9c73370547
Hardik Sharma <handy.sharma121@gmail.com>
2020-05-06 21:28:15 +0530

    Declared constructors for bigint

--- main.cpp
+++ main.cpp
@@ -2,12 +2,20 @@
 #include "bigint.h"
 using namespace std;
 
+float fac(float n)
+{
+	if(n < 3)
+		return n;
+	else
+		return n * fac(n-1);
+}
+
 int main()
 {
 	freopen("inputcpp.txt", "r", stdin);
 	freopen("outputcpp.txt", "w", stdout);
-	int a, b;
-	cin >> a >> b;
-	cout << a*b;
+	float a;
+	cin >> a;
+	cout << fac(a);
 	return 0;
 }
